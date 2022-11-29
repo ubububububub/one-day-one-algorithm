3087d147780b6bf351c9d325fab64eb76f237d50
ub <kimub1204@naver.com>
2022-11-29 17:05:37 +0900

    35. 구슬을 나누는 경우의 수 / 0 / O

---
+++ Kim Ubeom/0035.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n-1)
+
+
+def solution(balls, share):
+    return factorial(balls) // (factorial(balls - share) * factorial(share))
+
+
+print(solution(3, 2))
